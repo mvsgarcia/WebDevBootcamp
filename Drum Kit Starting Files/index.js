af01ea8c6971eb6d4edeb46a44f6f38f6be33096
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-20 13:13:15 +0800

    try changing the font color upon click

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,13 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+
+    this.style.color = "white";
+
+
   });
 }
+
+
+//    var audio = new Audio("sounds/tom-1.mp3");
+//    audio.play();
