56dcc7ba836bd076cc72b65a0b8863e0f19dabe0
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-23 15:35:54 +0800

    check button pressed

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,7 +2,15 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
-function nextSequence(){
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+ var userChosenColour = $(this).attr("id");
+ userClickedPattern.push(userChosenColour);
+ console.log(userClickedPattern);
+});
+
+function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
@@ -10,6 +18,7 @@ function nextSequence(){
 
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour ".mp3");
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
+
 }
