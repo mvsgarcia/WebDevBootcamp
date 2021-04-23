139e767292c61a201538e53c979f935b0014b96d
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-23 14:33:00 +0800

    get id and flash animation

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,4 +7,8 @@ function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  
 }
