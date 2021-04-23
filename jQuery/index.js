0923c57ac65a8199e43133a8902de5851e580e93
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-23 12:38:42 +0800

    keypress event listener

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,3 @@
-$("h1").text("Bye!");
-
-$("button").html("<em>Hey</em>");
-
-// $("button").text("<em>Hey</em>");   // may lalabas na em as text sa buttons unlike pag .html gamit na Hey lang 
+$(document).keypress(function(event) {
+  $("h1").text(event.key);
+});
