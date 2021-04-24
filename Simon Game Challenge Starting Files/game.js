2884ab99a1f23e9f74e271b2f0fc462dace85963
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-24 15:43:00 +0800

    added keyboard events

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,6 +4,20 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var started = false;
+
+var level = 0;
+
+$(document).keydown( function() {
+ if (!started){
+  
+  $("#level-title").text("Level " + level);
+  nextSequence(); 
+  started = true;
+ }
+  
+});
+
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
@@ -16,12 +30,17 @@ $(".btn").click(function() {
 
 function nextSequence() {
 
+  level++;
+
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+  
 }
 
 function playSound(name) {
