7b6ab8c3614c5b1e4f2b3b030763e791b740384b
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-28 15:11:43 +0800

    understanding routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get("/", function(request, response){
-    console.log(request);
+app.get("/", function(req, res){
+    //console.log(request);
+    res.send("Hello");
+})
+
+app.get("/contact", function (req, res) {
+  res.send("contact me at my-email@email.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("I'm Miecaela");
+});
+
+app.get("/hobbies", function(req, res){
+        res.send("<ul><li>coffee</li><li>games</li><li>code</li></ul>");
 })
 
 app.listen(3000, function() {
