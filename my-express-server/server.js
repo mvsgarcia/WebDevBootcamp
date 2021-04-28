e527d4d669b053a055132c663879dfeef27c6c79
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-28 14:35:47 +0800

    check request

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get("/", function(request, response){
+    console.log(request);
+})
+
 app.listen(3000, function() {
     console.log("Server added on port 3000");
 });
