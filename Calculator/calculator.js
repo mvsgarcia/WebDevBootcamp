68ac8f4f2618c9f34014fea3893aa583f4efffeb
mvsgarcia <miecaelagarcia@gmail.com>
2021-04-28 16:24:34 +0800

    Processing Post Requests with Body Parser

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,11 +1,23 @@
 //Setup express
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   //res.send("<h1>Hello World</h1>"); //Send the words Hello World from the root route as the response
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+
+    var result = n1 + n2;
+
+    res.send("The result of the calculation is " + result);
 });
 
 //Spin up our server on port 3000 with app.listen
